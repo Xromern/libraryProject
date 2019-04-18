a3bc98c370f54069d5f2c4fb99ab11e3e7b57006
Danil Ivasuk <da.ivasuk@gmail.com>
2019-04-18 19:17:32 +0300

    adding new controller

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    /**
+     * @Route("/library", name="library")
+     */
+    public function index()
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+}
